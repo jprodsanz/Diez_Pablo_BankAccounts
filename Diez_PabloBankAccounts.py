d7edc80c7679c564e4965732e197ad1c9ba60d26
Pablo X <107918221+jprodsanz@users.noreply.github.com>
2022-08-12 15:51:31 -0500

    Update Diez_PabloBankAccounts.py

--- Diez_PabloBankAccounts.py
+++ Diez_PabloBankAccounts.py
@@ -1,7 +1,7 @@
 class  BankAccount:
     accounts = []
 
-    def __init__(self,int_rate,balance):
+    def __init__(self, int_rate, balance):
         self.int_rate = int_rate
         self.balance = balance
         BankAccount.accounts.append(self)
