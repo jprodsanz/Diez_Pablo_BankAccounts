1757878fd2791e0369cb695f915ea6b4c16c7ba2
Pablo X <jprodsanz@gmail.com>
2022-08-12 14:01:41 -0500

    attempt 1

---
+++ Diez_PabloBankAccounts.py
@@ -0,0 +1,13 @@
+# Bank Account Assingment
+
+class  BankAccount: 
+
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance 
+    
+    def deposit(self, amount):
+        self.amount = amount 
+    
+    def withdraw(self, amount):
+        self.withdraw = amount
